d2f6ceb5d277d7360b7928517fed696b94788851
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2022-08-25 16:06:32 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -15,10 +15,10 @@
 
 // * / ** + -
 
-// food = Number(prompt('how much was the food?'))
-// tipPercentage = Number(prompt('tip %?')) / 100
-// tipAmount = food * tipPercentage
-// total = food + tipAmount
+ food = Number(prompt('how much was the food?'))
+ tipPercentage = Number(prompt('tip %?')) / 100
+ tipAmount = food * tipPercentage
+ total = food + tipAmount
 
  console.log('tip amount', tipAmount)
  console.log('total', total)
