193fb7b3b0513179769f1ee95903b20d81e1d070
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2022-08-26 19:32:17 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -290,3 +290,39 @@ const letterFrequency = (phrase)=>{
 }
 
 console.log(letterFrequency('haha'))
+
+//============= 2nd approach ====================
+
+// function getFrequency(string) {
+//     var freq = {};
+//     for (var i=0; i<string.length;i++) {
+//         var character = string.charAt(i);
+//         if (freq[character]) {
+//            freq[character]++;
+//         } else {
+//            freq[character] = 1;
+//         }
+//     }
+
+//     return freq;
+// };
+
+
+//============= Counting word frequency ============================
+let freqL = {};
+const wordFreq = (phrase) => {
+  let array = phrase.split(' ');
+  for (let str of array) {
+    if (str in freqL) {
+      freqL[str] += 1;
+    }
+    else {
+      freqL[str] = 1;
+    }
+  }
+  return freqL
+}
+
+userInput = prompt('Enter the sentence')
+
+console.log(wordFreq(userInput))
