7ff7e694cdc0605a703b7d8271f8a2e9a80d4b97
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2022-08-27 12:56:50 +0530

    Updated file

--- basic_2_0.js
+++ basic_2_0.js
@@ -1,3 +1,5 @@
+//Higher Order function - they take functions as input === map(),reduce(),filter()
+
 //Array methods
 //maps - loops and returns array
 const doubleMap = (numbers) => {
@@ -5,19 +7,33 @@ const doubleMap = (numbers) => {
 }
 
 console.log(doubleMap([1,2,3,4]))
-
+//===================================================================================
 //Filters ->The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that 
 //pass the test implemented by the provided function.
 const filters = (numbers, greaterThan) => {
   return numbers.filter(i => i > 3);
 }
-
 console.log(filters([1, 2, 3, 4, 5, 6]))
 
-const actors = [
-  {name: 'johnny', networth:2000000},
-  {name: 'amber' , networth:10},
-  {name: 'leonardo',networth:1000000}
+//===================================================================================
+//creating array of objects
+ const actors = [
+  { name: 'johnny🏴‍☠️', netWorth: 2000000 },
+  { name: 'amber🐕🐾🐝', netWorth: 10 },
+  { name: 'matt', netWorth: 170000000 },
+  { name: 'brad', netWorth: 300000000 },
+  { name: 'leonardo', netWorth: 10000000 },
 ]
 
-let result = (actors.filter(actor => actor.networth>10))
+ //filtering out actors with netW > 10
+let result = actors.filter(actor => actor.netWorth > 10)
+console.log(result)
+//with the help of the filtered result , extracting the names from the objects using map
+let names = result.map(actor => actor.name).join(', ')
+
+//reduce() -> use case for sum
+console.log(actors.reduce((prev, curr) => prev + curr.netWorth, 0))
+ playground.innerHTML = `<h1>${names}</h1>`
+ 
+ //===================================================================================
+ 
