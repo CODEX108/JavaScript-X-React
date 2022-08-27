2d930356fa564c0e889752c9af5878b224c468a8
Manjiri Chavande <82377810+CODEX108@users.noreply.github.com>
2022-08-27 12:17:07 +0530

    Added array methods

--- basic_2_0.js
+++ basic_2_0.js
@@ -1,1 +1,15 @@
-sdasd
+//Array methods
+//maps - loops and returns array
+const doubleMap = (numbers) => {
+  return numbers.map(i => i * 2);
+}
+
+console.log(doubleMap([1,2,3,4]))
+
+//Filters ->The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that 
+//pass the test implemented by the provided function.
+const filters = (numbers, greaterThan) => {
+  return numbers.filter(i => i > 3);
+}
+
+console.log(filters([1, 2, 3, 4, 5, 6]))
